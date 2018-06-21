c2af220051bc0ac7bbdc618c2772a40a1ec7378c
Shuai WANG <shuaiwang0121@gmail.com>
2018-06-21 19:01:32 +0800

    review finished

--- 1.TwoSum.cpp
+++ 1.TwoSum.cpp
@@ -1,15 +1,25 @@
-class Solution {
+class solution{
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> answer;
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = i + 1; j < nums.size(); ++j) {
-                if (nums[i] + nums[j] == target) {
-                    answer.push_back(i);
-                    answer.push_back(j);
-                    return answer;
+        vector <int> ans;
+        for (int i = 0；i < nums.size();i++){
+            for (int j = 0; j < nums.size(); j++){
+            if (nums[i] + nums [j] == target){
+                ans.push_back[i];
+                ans.push_back[j];
+                return ans;
                 }
             }
         }
     }
 };
+
+//note
+/* 1.indices指数、目录 本题中指在一个array中的位置序号
+   2.注意vector的使用语法，可以认为是一个动态数组
+   3.在需要用两个变量（可能跟时间复杂度有关？？）对整个vector进行遍历的时候，要想到两层for循环，思路是首先将一个变量（i)固定，使另一个变量（j)遍历到结尾，再逐渐移动第一个变量(i)，从而往复
+   4.i++与++i的区别：在for循环中没有区别
+   但++i是前量自增，先增加再赋值，比如int i = 0; a = ++i; a此时是1；
+   i++是后量自增，先赋值再增加，同样int i = 0; a = i++; a此时是0；
+   在大规模的循环中，++i更快，因为i++中系统需要保存一个临时变量保存i
+   */
