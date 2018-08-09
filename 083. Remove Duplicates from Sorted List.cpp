dfdeb7b49956d44579293ac7aaeaef08a5ba0586
Shuai WANG <shuaiwang0121@gmail.com>
2018-08-09 23:05:27 +0800

    Update 083. Remove Duplicates from Sorted List.cpp

--- 083. Remove Duplicates from Sorted List.cpp
+++ 083. Remove Duplicates from Sorted List.cpp
@@ -36,7 +36,12 @@ public:
 上面的语法表示为：
 p = p -> next;//p先移动到下一个位置a
 p -> val = p -> next ->val;//将a位置的下一位的值赋给p,因为最后一位指向NULL,因此最后会以NULL结尾
-
 3.指针的移动不可以用p++;
 4.判断指针是否为空，直接用p==NULL;
+5.在移动过程中，不仅要判断p->next是否为空，还要再判断p.因为p -> next = p-> next -> next;在p做移动之后，有可能p就是NULL，没有next
+因此不能把p到p->next的移动写在这一步，下面就是一个错误的实例：
+if(p -> val == p -> next ->val){
+     p -> next = p-> next -> next;
+     p = p -> next;
+     }
 */
