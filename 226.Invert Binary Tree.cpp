a58b3a4269e09c5636930e3af6d98e31887fec0e
Shuai WANG <shuaiwang0121@gmail.com>
2018-09-25 22:08:11 -0700

    Invert Binary Tree

---
+++ 226.Invert Binary Tree.cpp
@@ -0,0 +1,63 @@
+递归方法；
+T（O） = O（n), S(O) = O(h), h is the height of the tree
+思路：
+1.有关tree的问题都可以通过递归实现，注意边界条件，即当根结点不存在的以后返回NULL
+2.从上至下分解问题：
+只要把所有结点的左右子树对调，且把所有左子树和右子树的子树也进行对调即可
+3.从下往上返回结果：
+会从叶子结点开始依次向上返回，最后处理根结点左右结点，因此最终返回root即可
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        invertTree(root->left);
+        invertTree(root->right);
+        TreeNode* temp = root -> right;
+        root -> right = root ->left;
+        root -> left = temp;
+        return root;
+    }
+};
+
+遍历方法：前序遍历
+T（O） = O（n), S(O) = O(n)
+注意：
+1.queue的类型是TreeNode*
+2.遍历用到while判断条件的时候一般是queue不为空，当queue为空的时候可以return
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root) return NULL;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *head = q.front();
+            q.pop();
+            if(head->left) q.push(head->left);
+            if(head->right) q.push(head->right);
+            
+            TreeNode *temp = head ->right;
+            head -> right = head ->left;
+            head -> left = temp;
+        }
+        return root;
+    }
+};
