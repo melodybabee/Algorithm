3b0a3c44fb947957ee5e8e2295ef37d6e7e4cb77
Shuai WANG <shuaiwang0121@gmail.com>
2018-09-30 22:34:34 -0700

    Move Zeroes

---
+++ Array/283.Move Zeroes.cpp
@@ -0,0 +1,28 @@
+/*
+设计思路：
+1.遍历vector, 不是0的数字就放到vector的前面，等到所有不是0的数字都移动到前面之后，在末尾补0；
+
+注意：
+1.请你区分while和if！！！
+2.void方法不带返回值，直接return即可，隐形的return发生在最后一个函数结束以后，返回类型是void的函数使用return语句是为了引起函数的强制结束
+3.注意每道题目想清思路再写代码，理不清楚思路不要写代码。
+写完之后自己进行测试，把bug的原因和位置找出来，试图更正，必须要有思考的过程！！！
+如果完全没有思路或者思考后仍WA再看搜索别人的答案。
+*/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i]!= 0){
+                nums[ans] = nums[i];
+                ans++;
+            }
+        }
+        while(ans < nums.size()){
+            nums[ans]= 0;
+            ans++;
+        }
+        return;
+    }
+};
