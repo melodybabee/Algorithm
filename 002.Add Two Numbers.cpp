a8ffdcc2bab308f0977040a9832d25b2bd7f7107
Shuai WANG <shuaiwang0121@gmail.com>
2018-10-02 22:42:00 -0700

    Update 002.Add Two Numbers.cpp

--- 002.Add Two Numbers.cpp
+++ 002.Add Two Numbers.cpp
@@ -83,4 +83,61 @@ public:
     }
 };
 
-
+复习：
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode * ret = new ListNode(0);
+        ListNode *p = ret;
+        int num = 0;
+        int temp = 0;
+        
+        while(l1 && l2){
+            num = l1->val + l2 -> val + temp;
+            p -> next = new ListNode(num%10);
+            if(num > 9){//和ans = num/10;来判断进位的效果相同
+                temp = 1;
+            }else{
+                temp = 0;
+            }
+            l1 = l1 -> next;
+            l2 = l2 -> next;
+            p = p->next;
+        }
+        while (l1 && !l2){
+            num = l1 -> val + temp;
+            p -> next = new ListNode(num%10);
+            if(num > 9){
+                temp = 1;
+            }else{
+                temp = 0;
+            }
+            l1 = l1 -> next;
+            p = p->next;
+        }
+        while (!l1 && l2){
+            num = l2 -> val + temp;
+            p -> next = new ListNode(num%10);
+            if(num > 9){
+                temp = 1;
+            }else{
+                temp = 0;
+            }
+            l2 = l2 -> next;
+            p = p->next;
+        }
+        
+        if(temp == 1){
+            p -> next = new ListNode(temp);
+        }
+        return ret->next;
+    }
+};
