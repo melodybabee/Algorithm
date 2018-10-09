8f68cc031e742129ef5dd054ff66d18bf3fb8911
Shuai WANG <shuaiwang0121@gmail.com>
2018-10-08 23:03:40 -0700

    Regular Expression Matching

--- 010.Regular Expression Matching.cpp
+++ 010.Regular Expression Matching.cpp
@@ -50,4 +50,57 @@ public:
 *********************************************
 方法2:DP
 思路：
-1
+s不动，依次循环p,再循环s
+dp注意初始化，动态转移方程，由下至上解决问题
+1, If p.charAt(j) == s.charAt(i)；  
+   dp[i][j] = dp[i-1][j-1];
+   abc and abc
+2, If p.charAt(j) == '.'； 
+   dp[i][j] = dp[i-1][j-1];
+   abc and .bc
+3, If p.charAt(j) == '*': 
+   here are two sub conditions:
+               1   if p.charAt(j-1) != s.charAt(i); 如果为*，且*前字母与当前s位置的字母不同，需要用s当前字母和p*前字母再前一个比较
+               dp[i][j] = dp[i][j-2]  //in this case, a* only counts as empty
+               cab and cc*ab;
+               2   if p.charAt(j-1) == s.charAt(i) or p.charAt(j-1) == '.':
+                              dp[i][j] = dp[i-1][j]    //in this case, a* counts as multiple a 
+                              aaab and a*b;
+                           or dp[i][j] = dp[i][j-1]   // in this case, a* counts as single a
+                              aab and a*ab;
+                           or dp[i][j] = dp[i][j-2]   // in this case, a* counts as empty
+                              aab and ac*ab;
+注意：
+1.c++中bool默认值为false,因为需要初始化dp,因此实际上dp中数组有用的值是dp[1][1]-dp[s.size()][s.size()]的，那么对于dp[0][-]和dp[-][0]的位置仍然需要初始化
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        bool dp[s.size()+1][p.size()+1];
+        memset(dp,false,sizeof(dp));
+        //if(s.empty() || p.empty()) return false;
+        dp[0][0] = true;//初始化
+        for(int i = 0; i < p.size(); ++i){//aab,c*a*b
+            if(p[i] == '*' && dp[0][i-1]){
+                dp[0][i+1] = true;
+            }
+        }
+        for(int i = 0; i< s.size();++i){
+            for(int j = 0; j < p.size(); ++j){
+                if(s[i] == p[j]){//abc,abc
+                    dp[i+1][j+1] = dp[i][j];//动态转移方程
+                }
+                if(p[j] == '.'){//abc,a.c
+                    dp[i+1][j+1] = dp[i][j];
+                }
+                if(p[j] == '*'){
+                    if(p[j-1] != s[i] && p[j-1] != '.'){//aab,b*b
+                        dp[i+1][j+1] = dp[i+1][j-1];
+                    }else{//aab, a*b
+                        dp[i+1][j+1] = (dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]);
+                    }
+                }
+            }
+        }
+        return dp[s.size()][p.size()];
+    }
+};
