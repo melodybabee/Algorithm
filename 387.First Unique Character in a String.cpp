ba5187bd377e63eaeffa3c2aefd8ac09dbc1c1cc
Shuai WANG <shuaiwang0121@gmail.com>
2018-10-11 14:09:28 -0700

    First Unique Character in a String

--- 387.First Unique Character in a String.cpp
+++ 387.First Unique Character in a String.cpp
@@ -1,33 +1,37 @@
+题意：
+找到string中第一个只出现一次的元素
+思路：
+1.建立hashtable,key中放元素，value放出现次数
+2.首先循环一遍把每个字符都放到map中，学会使用自动循环for(auto c: s)，无需再重新声明类型
+3.再重新循环string,如果找到出现次数为1的位置，直接输出
 class Solution {
 public:
     int firstUniqChar(string s) {
-        if(s == "") return -1;
-        
-        for(int i = 0; i < s.size(); ++ i){
-            bool flag = true;
-            for (int j = 0; j < s.size(); ++j){
-                if(j == i) continue;
-                if(s[j] == s[i])
-                    flag = false;
-            }
-        
-            if(flag) return i;
+        unordered_map<char,int>map;
+        for(auto c: s) ++map[c];
+        for(int i = 0; i < s.size(); ++i){
+            if(map[s[i]] == 1)
+                return i;
         }
-        
         return -1;
     }
 };
 
-/*
-设计思路：
-1.用两个指针位于string头部,一个指针用于定位当前位，一个指针用于循环整个string
-2.每次新进入一个循环，设置标识位为ture,一旦有元素相等，标识位为false
-3.因为每次都会循环到自身，因此需要排除i==j的情况
-
-注意：
-1.不能只循环当前位置以后的元素
-2.需要跳过当前位置
-
 优化：
-在数据量很大的时候会TLE，需要优化
-*/
+如果不想用map,想直接使用vector的话，需要把字母转换为26进制数字的思路
+注意：
+1.如果想使用vector的index，必须在最初声明的时候就标号大小
+2.注意在第二个for循环中，仍然需要从string的第一位开始判断，因为想要找的是第一个出现一次的元素
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> vec(26,0);
+        for(int i = 0; i< s.size(); ++i){
+            vec[s[i] - 'a']++;
+        }
+        for(int i = 0; i < s.size(); ++i){
+            if(vec[s[i] - 'a'] == 1) return i;
+        }
+        return -1;
+    }
+};
