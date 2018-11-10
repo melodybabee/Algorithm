9dc06d270995beac8629d4be60a29f10f8024e59
Shuai WANG <shuaiwang0121@gmail.com>
2018-11-10 14:14:22 -0800

    Minimum Path Sum

--- DP/064.Minimum Path Sum.cpp
+++ DP/064.Minimum Path Sum.cpp
@@ -21,3 +21,22 @@ public:
         return grid[m-1][n-1];
     }
 };
+
+二刷：
+注意DP的问题能在远数组基础上修改就不要新建一个vector，初始化和边界容易出错。
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        for(int i = 1; i < m; ++i) grid[i][0] +=grid[i-1][0];
+        for(int j = 1; j < n; ++j) grid[0][j] += grid[0][j-1];
+        for(int i = 1; i < m ;++ i){
+            for(int j = 1; j < n; ++j){
+                grid[i][j] = min(grid[i-1][j]+grid[i][j], grid[i][j-1] + grid[i][j]);
+            }
+        }
+        return grid[m-1][n-1];
+        
+    }
+};
