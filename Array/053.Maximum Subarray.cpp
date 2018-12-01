c0a503717373bde67003776b1bef06635154bba2
Shuai WANG <shuaiwang0121@gmail.com>
2018-11-30 21:32:30 -0800

    Maximum Subarray

--- Array/053.Maximum Subarray.cpp
+++ Array/053.Maximum Subarray.cpp
@@ -55,6 +55,39 @@ public:
 1.因为ret需要通过每位进行比较，并存储下来，因此需要创建一个新的vector
 2.如果vector用到下标，在声明的时候就要声明出元素，如果vector为空是不能用到下角标的，可以先都声明为0
 
-
-
 */
+
+三刷：
+注意：
+1.本题的关键之处就在于如果过程中的和小于0，那么把求和的结果更改为0，重新计算，因为如果是负数那么会越加越小。
+2.负数直接舍弃就可以，因为如果能够在负数的基础上变大，一定是正数相加，而再相加也不如直接返回一个正数的值大。
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ret = nums[0];
+        int sum = 0;
+        for(int i = 0; i< nums.size(); ++i){
+            sum += nums[i];
+            ret = max(ret,sum);
+            if(sum < 0) sum = 0;
+        }
+        return ret;
+    }
+};
+
+DP:
+1.dp[i]表示[0,i]位置上能够获得的最大值，动态转移方程是当前的值，与当前的值+之前的最大值，两者之间的较大值。
+2.注意因为在更新dp的过程中会使结果减小，因此需要一个结果值来记录过程中产生的最大值，最后返回。
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int>dp(nums.size());
+        dp[0] = nums[0];
+        int ret = nums[0];
+        for(int i = 1; i < nums.size();++i){
+            dp[i] = max(dp[i-1]+nums[i],nums[i]);
+            ret = max(dp[i],ret);
+        }
+        return ret;
+    }
+};
