eb6c4cff85bf1f3908800e12cc854d6c6d3d1f8b
Shuai WANG <shuaiwang0121@gmail.com>
2018-12-01 16:47:01 -0800

    Update 123.Best Time to Buy and Sell Stock III.cpp

--- 123.Best Time to Buy and Sell Stock III.cpp
+++ 123.Best Time to Buy and Sell Stock III.cpp
@@ -28,6 +28,22 @@ public:
 };
 
 优化：可以优化为一维数组。【待学习】
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.empty()) return 0;
+        vector<int>local(3,0);
+        vector<int>global(3,0);
+        for(int i = 0; i < prices.size()-1;++i){
+            int diff = prices[i+1]- prices[i];
+            for(int j = 2; j >= 1; --j){
+                local[j] = max(global[j-1] + max(diff,0), local[j]+diff);
+                global[j] = max(global[j],local[j]);
+            }
+        }
+        return global[2];
+    }
+};
 
 补充：
 1.DP汇总
