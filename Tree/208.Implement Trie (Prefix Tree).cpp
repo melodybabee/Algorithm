519d67243614422656e4a8c9e05c145505f0cda6
Shuai WANG <shuaiwang0121@gmail.com>
2018-12-11 14:43:55 -0800

    Implement Trie (Prefix Tree)

--- Tree/208.Implement Trie (Prefix Tree).cpp
+++ Tree/208.Implement Trie (Prefix Tree).cpp
@@ -78,3 +78,69 @@ public:
  * bool param_2 = obj.search(word);
  * bool param_3 = obj.startsWith(prefix);
  */
+
+12.11复习：
+Trie树复习：
+注意：
+1.声明结构体要有；，写完属性要进行初始化，isword是一个变量因此：置为false即可，对于它的child要置为NULL
+2.没进入一层方法就定义一个p指针
+3.isword表示以该字母为结点的单词是否是一个单词
+class TrieNode{
+public:
+    TrieNode *child[26];
+    bool isWord;
+    TrieNode() : isWord(false){
+        for(auto &a : child) a = NULL;
+    }
+};
+
+class Trie {
+private:
+    TrieNode *root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode *p = root;
+        for(auto &a: word){
+            int i = a - 'a';
+            if(!p -> child[i]) p -> child[i] = new TrieNode();
+            p = p -> child[i];
+        }
+        p -> isWord = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode *p = root;
+        for(auto &a :word){
+            int i = a - 'a';
+            if(!p -> child[i]) return false;
+            p = p -> child[i];
+        }
+        return p -> isWord;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode *p =root;
+        for(auto &a : prefix){
+            int i = a - 'a';
+            if(!p -> child[i]) return false;
+            p = p -> child[i];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * bool param_2 = obj.search(word);
+ * bool param_3 = obj.startsWith(prefix);
+ */
