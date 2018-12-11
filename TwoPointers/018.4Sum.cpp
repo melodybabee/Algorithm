e5022b17ba8f8c1742171d7f8dfe2b44bb0e6c6e
Shuai WANG <shuaiwang0121@gmail.com>
2018-12-10 20:12:20 -0800

    4Sum

--- TwoPointers/018.4Sum.cpp
+++ TwoPointers/018.4Sum.cpp
@@ -33,3 +33,38 @@ public:
         return ret;
     }
 };
+
+12.10复习：
+注意：
+1.每一位都要判断是否相等。
+2.注意nums为空等等需要返回{}的情况。
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        if(nums.empty()) return {};
+        if(nums.size() <= 2) return {};
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for(int i = 0 ; i < nums.size()-3;++i){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i+1; j < nums.size();++j){
+                if(j> i+1 && nums[j] == nums[j-1]) continue;
+                int temp = target - nums[i] - nums[j];
+                int left = j+1;
+                int right = nums.size()-1;
+                while(left < right){
+                    if(nums[left] + nums[right] == temp){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        while(left < right && nums[left] == nums[left+1]) ++left;
+                        while(left < right && nums[right] == nums[right-1]) --right;
+                        ++left;
+                        --right;
+                    }else if(nums[left] + nums[right] < temp){
+                        ++left;
+                    }else --right;
+                }
+            }
+        }
+        return ans;
+    }
+};
