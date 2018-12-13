46bf1f30e786960d19f700034434a33dfc433a7d
Shuai WANG <shuaiwang0121@gmail.com>
2018-12-13 10:55:23 -0800

    Nested List Weight Sum

--- DFS/339.Nested List Weight Sum.cpp
+++ DFS/339.Nested List Weight Sum.cpp
@@ -101,3 +101,51 @@ public:
         return ret;
     }
 };
+
+12.13复习：
+注意：
+DFS思想因此需要记录下来level层数，再在递归函数中进行遍历。
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Constructor initializes an empty nested list.
+ *     NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     NestedInteger(int value);
+ *
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     void add(const NestedInteger &ni);
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+class Solution {
+public:
+    int depthSum(vector<NestedInteger>& nestedList) {
+        int level = 1;
+        return helper(nestedList,level);
+    }
+    
+    int helper(vector<NestedInteger>& nestedList,int level){
+        int ret = 0;
+        for(auto nest : nestedList){
+            ret += nest.isInteger() ? nest.getInteger()*level : helper(nest.getList(), level +1);
+        }
+        return ret;
+    }
+};
