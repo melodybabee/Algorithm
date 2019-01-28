0283c7ae0da8c3529023227b99ce16b846c2d415
Shuai WANG <shuaiwang0121@gmail.com>
2019-01-28 13:37:45 -0800

    Merge Sorted Array

--- TwoPointers/088.Merge Sorted Array.cpp
+++ TwoPointers/088.Merge Sorted Array.cpp
@@ -101,3 +101,39 @@ public:
         }
     }
 };
+
+1.28复习
+双指针做法：
+注意：
+1.因为有可能有负数存在的情况，所以p1,p2都需要单独判断一下是否已经小于0，并单独处理
+其中p1,p2都可以通过m,n来进行表示。
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int cur = m+n-1;
+        int p1 = m-1;
+        int p2 = n-1;
+        while(p2 >= 0 && p1 >= 0){
+            if(nums2[p2] >= nums1[p1]){
+                nums1[cur] = nums2[p2];
+                --p2;
+                --cur;
+            }else if(nums2[p2] < nums1[p1]){
+                nums1[cur] = nums1[p1];
+                --p1;
+                --cur;
+            }
+        }
+        while(p2 >= 0){
+            nums1[cur] = nums2[p2];
+            --p2;
+            --cur;
+        }
+        while(p1 >= 0){
+            nums1[cur] = nums1[p1];
+            --p1;
+            --cur;
+        }
+        
+    }
+};
