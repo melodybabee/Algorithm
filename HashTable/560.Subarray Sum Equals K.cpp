4b24563c5c9d8525717e96f0cb6db772c73db182
Shuai WANG <shuaiwang0121@gmail.com>
2019-02-01 16:12:44 -0800

    Subarray Sum Equals K

--- HashTable/560.Subarray Sum Equals K.cpp
+++ HashTable/560.Subarray Sum Equals K.cpp
@@ -26,7 +26,7 @@ public:
 T（n）= O（n）;
 思路：
 1.首先建立一个hashtable,key放入连续子数组之和，value放入出现的频次。
-2.求出连续的子数组和sun,想找到和等于k的值，那么可以快速查找map中是否有值等于sum-k,如果存在,那么一定也有和等于k,个数和sum-k的个数相等。
+2.求出连续的子数组和sum,想找到和等于k的值，那么可以快速查找map中是否有值等于sum-k,如果存在,那么一定也有和等于k,个数和sum-k的个数相等。
 3.同时把新的子数组的和放入map中
 class Solution {
 public:
@@ -97,5 +97,44 @@ public:
             if(sum == k) ++ ret;
         }
         return ret;
+    }
+};
+
+2.1复习：
+注意：
+two pointers的方法虽然快，但是如果有0或者负数的话就不能用，因为有可能会有和0的情况，那么两个指针重合的时候一定等于0，所以不能得到结果。
+但是如果都是正数，那么和不可能是0，因此可以直接two pointers比较
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int left = 0;
+        int count = 0;
+        int sum = 0;
+        for(int r = 0; r < nums.size(); ++r){
+            sum += nums[r];
+            while(sum > k){
+                sum -= nums[left];
+                ++left;
+            }
+            if(sum == k) ++count;
+        }
+        return count;
+    }
+};
+所以需要用到一个hashtable,用来记录数组的前缀和。如果在map中有子数组等于sum-k,那么也一定有和等于k，那么直接相加即可。
+注意因为有sum==k的情况，所以在map中需要插入{0,1}这个key-value,最后返回计数器的数量
+前缀和保证了数组一定是连续的。
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int>map{{0,1}};
+        int count = 0;
+        int sum = 0;
+        for(int r = 0; r < nums.size(); ++r){
+            sum += nums[r];
+            count += map[sum-k];
+            ++map[sum];
+        }
+        return count;
     }
 };
