efba3cbb7f758504410f214e31439bfabeb305d3
Shuai WANG <shuaiwang0121@gmail.com>
2019-02-03 19:01:23 -0800

    Basic Calculator III

--- Stack/772.Basic Calculator III.cpp
+++ Stack/772.Basic Calculator III.cpp
@@ -45,3 +45,46 @@ public:
         return ret;
     }
 };
+
+2.3复习：
+注意：
+1.可以不用switch...case...break来判断，但是要注意写清楚if...else
+2.在进入递归的时候要从当前停下的位置(j)的下一位开始进入递归
+class Solution {
+public:
+    int calculate(string s) {
+        int ret = 0;
+        int num = 0;
+        int cur = 0;
+        char op = '+';
+        for(int i = 0; i < s.size(); ++i){
+            if(s[i] -'0' >= 0){
+                num = 10*num + s[i] -'0';
+            }
+            if(s[i] == '('){
+                int count = 0;
+                int j = i;
+                for(i; i< s.size();++i){
+                    if(s[i] == '(') ++count;
+                    if(s[i] == ')') --count;
+                    if(count == 0)break;
+                }
+                num = calculate(s.substr(j+1,i-j+1));
+            }
+            if((s[i] - '0' < 0 && s[i] != ' ')|| i == s.size()-1){
+                if(op == '+') cur += num;
+                else if(op == '-') cur -= num;
+                else if(op == '*') cur *= num;
+                else if(op == '/') cur /= num;
+                
+                if(s[i] == '+' || s[i] == '-' || i == s.size()-1){
+                    ret += cur;
+                    cur = 0;
+                }
+                op = s[i];
+                num = 0;
+            }
+        }
+        return ret;
+    }
+};
