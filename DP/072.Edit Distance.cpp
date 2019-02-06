0e98ce9e267c992d04817c25def7868919755871
Shuai WANG <shuaiwang0121@gmail.com>
2019-02-05 23:59:24 -0800

    Edit Distance

--- DP/072.Edit Distance.cpp
+++ DP/072.Edit Distance.cpp
@@ -41,3 +41,34 @@ public:
         return vec[m][n];
     }
 };
+
+2.4复习
+注意：
+1.首先DP需要先声明dp[i][j]的含义，本题的意义是表示word1(0,i)与word2(0,j)之间最少有几步才能匹配上
+2.因为坐标从0开始，所以在过程中要注意坐标的变化
+（1）初始化的时候从0开始
+（2）但是双层遍历的时候从1开始遍历，但是比较要从字符串的第一位开始，因此是word1[i-1] == word2[j-1]
+（3）因为从1开始，因此判断条件需要等于单词的长度
+（4）最后返回的时候要返回单词的长度，才是DP数组的最后一位，或者直接返回back().back()
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1));
+        for(int i = 0; i <= word1.size();++i){
+            dp[i][0] = i;
+        }
+        for(int i = 0; i <= word2.size(); ++i){
+            dp[0][i] = i;
+        }
+        for(int i = 1; i <= word1.size(); ++i){
+            for(int j = 1; j <= word2.size(); ++j){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = min(min(dp[i-1][j-1],dp[i-1][j]),dp[i][j-1])+1;
+                }
+            }
+        }
+        return dp.back().back();
+    }
+};
