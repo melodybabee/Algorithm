de4eeb785379e2ef19886dd31dc0b4c93b75c56c
Shuai WANG <shuaiwang0121@gmail.com>
2019-02-20 22:19:25 -0800

    Recover Binary Search Tree

--- Tree/099.Recover Binary Search Tree.cpp
+++ Tree/099.Recover Binary Search Tree.cpp
@@ -111,3 +111,99 @@ public:
         if(first && second) swap(first->val,second -> val);
     }
 };
+
+2.20复习：
+注意：
+1.因为题目中说了是两个元素，所以设置两个指针找到两个元素，进行交换即可
+first second作为交换的两个元素，cur表示当前遍历到的元素，进行中序遍历
+2.注意只有当first不存在的时候才把first赋值，说明这个元素的位置一定不对。但是它与哪个元素进行交换要在遍历之后才能得出，second表示的是需要与它交换的位置。
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *first = NULL;
+    TreeNode *second = NULL;
+    TreeNode *cur = NULL;
+    void recoverTree(TreeNode* root) {
+        recover(root);
+        if(first && second) swap(first->val,second->val);
+    }
+    
+    void recover(TreeNode *root){
+        if(!root) return;
+        recover(root->left);
+        if(!cur){
+            cur = root;
+        }else{
+            if(cur -> val > root->val){
+                // 注意此处需要判断first,只有不存在的时候才进行赋值，表示这个值的位置不对，后面需要找到与它交换的位置，即后面比cur更小的值
+                if(!first)first = cur;
+                second = root;
+            }
+            cur = root;
+        }
+        recover(root->right);
+    }
+};
+方法2:线索二叉树
+注意：
+1.first仍然表示第一个位置不对需要向后换的元素，second表示后面较小的元素
+parent表示当前需要和遍历到的结点比较大小的结点位置
+pre用来对树进行预处理，即把树进行中序遍历连接在一起
+2.注意在预处理过程中，分清楚是右结点不存在了还是已经和下一个位置连接在一起了，需要分情况讨论
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode *cur = root;
+        TreeNode *pre = NULL;
+        TreeNode *first = NULL;
+        TreeNode *second = NULL;
+        TreeNode *parent = NULL;
+        while(cur){
+            if(!cur->left){
+                if(parent && parent -> val > cur->val){
+                    if(!first) first = parent;
+                    second = cur;
+                }
+                parent = cur;
+                cur = cur->right;
+            }else{
+                pre = cur->left;
+                while(pre->right && pre->right != cur){
+                    pre = pre ->right;
+                }
+                if(!pre->right){
+                    pre->right = cur;
+                    cur = cur ->left;
+                }else{
+
+                    pre->right = NULL;
+                    if(parent && parent -> val > cur->val){
+                        if(!first) first = parent;
+                        second = cur;
+                    }
+                    parent = cur;
+                    cur = cur->right;
+                }
+            }
+        }
+        
+        if(first && second) swap(first->val,second->val);
+    }
+};
