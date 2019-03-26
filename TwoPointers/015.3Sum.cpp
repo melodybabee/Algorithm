21933a2a870cf50c1d32314249419a63222646c6
Shuai WANG <shuaiwang0121@gmail.com>
2019-03-25 23:49:51 -0700

    3Sum

--- TwoPointers/015.3Sum.cpp
+++ TwoPointers/015.3Sum.cpp
@@ -125,3 +125,34 @@ public:
         return result;
     }
 };
+
+3.25复习：
+注意：
+1.注意去重，一个是标准位置上的去重，一个是两个指针的去重
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>res;
+        sort(nums.begin(),nums.end());
+        for(int i = 0; i < nums.size(); ++i){
+            if(i-1 >= 0 && nums[i] == nums[i-1]) continue;
+            int cur = -nums[i];
+            int l = i+1;
+            int r = nums.size()-1;
+            while(l < r){
+                if(nums[l] + nums[r] == cur){
+                    res.push_back({nums[i],nums[l],nums[r]});
+                    ++l;
+                    while(l < r && nums[l] == nums[l-1]) ++l;
+                    --r;
+                    while(l < r && nums[r] == nums[r+1]) --r;
+                }else if(nums[l] + nums[r] > cur){
+                    --r;
+                }else{
+                    ++l;
+                }
+            }
+        }
+        return res;
+    }
+};
