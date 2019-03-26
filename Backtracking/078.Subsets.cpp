1688edc1b0d9a1796e779d5b369ac0e048adc6d0
Shuai WANG <shuaiwang0121@gmail.com>
2019-03-25 17:35:28 -0700

    Subsets

--- Backtracking/078.Subsets.cpp
+++ Backtracking/078.Subsets.cpp
@@ -44,4 +44,51 @@ public:
         }
     }
 };
-方法三：位运算方法待学习
+
+3.25复习：
+回溯法【重点复习】
+注意：
+1.在helper函数中如何判断是否到了边界,就是每次首先push进去temp数组，因此也会push进去空数组
+2.要设置一个起点位置，每次遍历都是从起点位置开始，但是递归的部分是从当前遍历到的位置+1进行下一步递归的
+3.回溯思想的核心是要有pop_back()的过程，一旦结束了上一次递归，那么回回到调用递归的函数位置，继续下一步，即弹出的操作
+同时再次进入遍历中，如果没到结尾push进去下一位数字
+所以[1,2,3]的输出顺序是[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ret;
+        vector<int> temp;
+        helper(ret,temp,nums,0);
+        return ret;
+    }
+    
+    void helper(vector<vector<int>> &ret, vector<int> &temp, vector<int>& nums, int start){
+        ret.push_back(temp);
+        for(int i = start; i < nums.size(); ++i){
+            temp.push_back(nums[i]);
+            helper(ret,temp,nums,i+1);
+            temp.pop_back();
+        }
+    }
+};
+
+两层递归方法：
+不断更新数组
+注意：
+要点在于先把之前的数组复制，再更新进去新的值。
+复制的过程就是内层循环完成的，记录当前的数组大小，在结果数组的尾部从头部开始push_back进去原数组。
+再在每次新更新的子数组中加入新的元素。
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ret {{}};
+        for(int i = 0; i < nums.size(); ++i){
+            int size = ret.size();
+            for(int j= 0; j < size; ++j){
+                ret.push_back(ret[j]);
+                ret.back().push_back(nums[i]);
+            }
+        }
+        return ret;
+    }
+};
